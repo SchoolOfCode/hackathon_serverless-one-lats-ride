e2f7c50d78fc09ca7a7a07e9cead323f1315fb26
= <=>
2020-12-11 15:51:53 +0000

    delete item working

---
+++ aws-hackathon/Handler/deleteItem.js
@@ -0,0 +1,23 @@
+"use strict";
+const { IdentityStore } = require("aws-sdk");
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2019.11.21" });
+const bootcamperTable = process.env.TABLE;
+const response = require("../handler");
+
+module.exports.deleteItem = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    key: {
+      id: id,
+    },
+    TableName: bootcamperTable,
+  };
+  return db
+    .delete(params)
+    .promise()
+    .then(() =>
+      callback(null, response(200, { message: `${id} deleted successfully` }))
+    )
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
--- aws-hackathon/handler.js
+++ aws-hackathon/handler.js
@@ -1,16 +1,14 @@
-'use strict'; //enables strict mode, which makes things that normally cause
+"use strict"; //enables strict mode, which makes things that normally cause
 //warnings error out (keeps the code cleaner)
-const AWS = require('aws-sdk'); //requires AWS CLI that you set up with your credentials earlier
-const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2019.11.21' }); //creates a
+const AWS = require("aws-sdk"); //requires AWS CLI that you set up with your credentials earlier
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2019.11.21" }); //creates a
 //new instance of DynamoDB when called using the AWS SDK
-const { v4: uuidv4 } = require('uuid'); //auto-generates unique ids
+const { v4: uuidv4 } = require("uuid"); //auto-generates unique ids
 
 const bootcamperTable = process.env.TABLE;
 
 //---------HELPER FUNCTION TO SEND RESPONSE JSONS WITH HEADERS:---------
-
-//This saves you from having to do these bits to process the response in each function :)
-module.exports.response = (statusCode, message) => {
+const response = (statusCode, message) => {
   //takes in the status code and a message (an object) from the response
   return {
     statusCode: statusCode,
@@ -18,12 +16,15 @@ module.exports.response = (statusCode, message) => {
     headers: {
       //sticks all the right headers on to talk to the request during the
       //preflight check (CORS)
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET, OPTIONS, POST, PUT, DELETE',
-      'Content-Type': 'application/json',
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "GET, OPTIONS, POST, PUT, DELETE",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(message), //stringifies the body object into JSON
   };
 };
+
+//This saves you from having to do these bits to process the response in each function :)
+module.exports = response;
